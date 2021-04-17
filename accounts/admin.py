3d9ac2190ce14a0a37a683cb0261d7f02a97708b
Mo17 <mo.err17@gmail.com>
2021-04-17 14:40:54 +0200

    register models to the admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Account, PremiumToolOwnership
+
+admin.site.register(Account)
+admin.site.register(PremiumToolOwnership)
