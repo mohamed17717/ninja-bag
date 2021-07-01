07066f1f8ec34db83c6f69a50ea11a50477a540b
Mo17 <mo.err17@gmail.com>
2021-07-01 09:12:06 +0200

    notifictaion styled better

--- static/js/main.js
+++ static/js/main.js
@@ -14,7 +14,7 @@ function notify(msg, type){
   const colors = {success: 'bg-green-700', failed: 'bg-red-700', error: 'bg-red-700'}
   const color = colors[type]
   const elm = document.createElement('div')
-  const classes = `notification fixed ${color} py-3 px-5 text-center right-0 bottom-10 w-32 capitalize text-white font-bold opacity-70 transition transform translate-x-full`
+  const classes = `notification fixed ${color} py-3 px-5 text-center right-0 bottom-10 capitalize text-white font-bold opacity-70 transition transform translate-x-full`
   elm.setAttribute('class', classes)
   elm.innerText = msg
 
