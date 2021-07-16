8000e78a85b10e1f998279afeb2a6ec25452a5b5
Mo17 <mo.err17@gmail.com>
2021-07-16 02:48:09 +0200

    remove premium And ownership class

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 
-from .models import Account, PremiumToolOwnership
+from .models import Account
 
 admin.site.register(Account)
-admin.site.register(PremiumToolOwnership)
