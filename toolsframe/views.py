49c5a7bd3b64b45a9388500b901c7e70c4b991e8
Mo17 <mo.err17@gmail.com>
2021-07-17 02:40:35 +0200

    show tools without limitation and stop redirects

--- toolsframe/views.py
+++ toolsframe/views.py
@@ -10,17 +10,22 @@ from classes.Redirector import Redirector
 from handlers import ToolHandler
 
 def get_default_context(request):
-  return {
-    'user': request.user,
-    'account': request.user.user_account.get(),
+  is_authenticated = request.user.is_authenticated
+  context = {
     'upcoming_tools': UpcomingTool.list_all_active(),
-    'is_limits_active': ToolHandler.is_limits_active
+    'is_limits_active': ToolHandler.is_limits_active,
+    'is_authenticated': request.user.is_authenticated
   }
+
+  if is_authenticated:
+    context.update({ 'account': request.user.user_account.get() })
+
+  return context
 
 @require_http_methods(['GET'])
 def index(request):
-  if not request.user.is_authenticated:
-    return Redirector.go_login()
+  # if not request.user.is_authenticated:
+  #   return Redirector.go_login()
 
   context = {
     **get_default_context(request),
