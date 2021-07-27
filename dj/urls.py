7aee3e363af698256e12e28855a1df35239e0c25
Mo17 <mo.err17@gmail.com>
2021-07-27 05:52:55 +0200

    make tools endpoint to the root path /tool-endpoint instead of /t/endpoint

--- dj/urls.py
+++ dj/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
   path('logout/', LogoutView.as_view(template_name=settings.LOGOUT_REDIRECT_URL), name='logout'),
 
   path('', include('toolsframe.urls', namespace='toolsframe')),
-  path('t/', include('tools.urls', namespace='tools')),
+  path('', include('tools.urls', namespace='tools')),
   path('account/', include('accounts.urls', namespace='accounts')),
 
   path('admin/', admin.site.urls),
