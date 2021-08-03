a0ea2cbce78fb2ef4fe4aeccba923f8b47b929ce
Mo17 <mo.err17@gmail.com>
2021-08-03 07:24:30 +0200

    show tables in admin dashboard

--- toolsframe/admin.py
+++ toolsframe/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 
-from .models import Category, Tool, UpcomingTool, SuggestedTool, ToolIssueReport, ToolViewsFunctions
+from .models import Category, Tool, UpcomingTool, SuggestedTool, ToolIssueReport, ToolViewsFunctions, ToolDatabaseClass
 
 admin.site.register(Category)
 admin.site.register(Tool)
 admin.site.register(UpcomingTool)
 admin.site.register(SuggestedTool)
 admin.site.register(ToolIssueReport)
-# admin.site.register(ToolViewsFunctions)
+
+admin.site.register(ToolViewsFunctions)
+admin.site.register(ToolDatabaseClass)
