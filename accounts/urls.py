2555feeb2701fc1f2eabed7e453f9962cbb36294
Mo17 <mo.err17@gmail.com>
2021-08-04 02:51:07 +0200

    remove unused code

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,8 +3,6 @@ from django.urls import path
 
 from .views import (
   index,
-  register_account,
-  login_account,
   logout_account
 )
 
@@ -13,7 +11,5 @@ app_name = 'accounts'
 
 urlpatterns = [
   path('', index, name='auth'),
-  path('register/', register_account.as_view(), name='register'),
-  path('login/', login_account.as_view(), name='login'),
   path('logout/', logout_account, name='logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import View
-from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth import logout
 from django.contrib import messages
-from django.contrib.auth.forms import AuthenticationForm
-
-from .forms import RegisterAccountForm
 
 from classes.Redirector import Redirector
 
@@ -20,51 +16,3 @@ def logout_account(request):
   messages.info(request, "You have successfully logged out.") 
   return Redirector.go_login()
 
-
-class register_account(View):
-  def get(self, request, form=None):
-    if request.user.is_authenticated:
-      return Redirector.go_home()
-
-    context = { 'register_form': form or RegisterAccountForm }
-    return render(request, 'register.html', context)
-
-
-  def post(self, request):
-    if request.user.is_authenticated:
-      return Redirector.go_home()
-
-    form = RegisterAccountForm(request.POST)
-    if form.is_valid():
-      user = form.save()
-      login(request, user)
-      messages.success(request, 'Registration successful.' )
-      return Redirector.go_home()
-
-    messages.error(request, 'Unsuccessful registration. Invalid information.')
-    return self.get(request, form)
-
-
-class login_account(View):
-  def get(self, request, form=None):
-    if request.user.is_authenticated:
-      return Redirector.go_home()
-
-    context = { 'login_form': form or AuthenticationForm() }
-    return render(request, 'login.html', context)
-
-  def post(self, request):
-    if request.user.is_authenticated:
-      return Redirector.go_home()
-
-    form = AuthenticationForm(request, data=request.POST)
-    if form.is_valid():
-      username = form.cleaned_data.get('username')
-      password = form.cleaned_data.get('password')
-      user = authenticate(username=username, password=password)
-      if user is not None:
-        login(request, user)
-        return Redirector.go_home()
-
-    messages.error(request,'Invalid username or password.')
-    return self.get(request, form)
