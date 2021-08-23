f45c9a0705bc89fa4c3cc9a328a14ac9f577a80c
Mo17 <mo.err17@gmail.com>
2021-08-23 22:05:34 +0200

    remove unused view

--- tools/urls.py
+++ tools/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 from .views import (
-  index,
   get_my_ip,
   get_my_proxy_anonimity,
   get_my_request_headers,
@@ -25,8 +24,6 @@ app_name = 'tools'
 
 
 urlpatterns = [
-  path('', index, name='tools-home'),
-
   path('get-my-ip/', get_my_ip, name='get-my-ip'),
 
   path('get-my-proxy-anonimity/', get_my_proxy_anonimity, name='get-my-proxy-anonimity'),
