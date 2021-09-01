42c7981b7238100440d7b3510eb6c06344e2d71c
Mo17 <mo.err17@gmail.com>
2021-09-01 10:30:17 +0200

    make error page dynamically return response ethier for browser or api using the user-agent tool

--- app/views.py
+++ app/views.py
@@ -1,12 +1,20 @@
 from django import template
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, HttpResponse
 from django.template import RequestContext
-
+from tools.controller import RequestAnalyzerTools
 
 def error_page(number):
   template_name = 'error-page.html'
   def view_function(request, *args, **kwargs):
-    response = render_to_response(template_name, {'number': number})
-    response.status_code = number
+    ua = request.META['HTTP_USER_AGENT']
+    ua_details = RequestAnalyzerTools.get_user_agent_details(ua)
+
+    is_from_api = not any(ua_details['flags'].values()) # all must be false
+    if is_from_api:
+      response = HttpResponse(status=number)
+    else:
+      response = render_to_response(template_name, {'number': number})
+      response.status_code = number
+
     return response
   return view_function
