716a3ecdf6505b50012b0cdbc913719e73363a35
Mo17 <mo.err17@gmail.com>
2021-09-10 14:38:02 +0200

    rewrite tools of get_ip and check anonimity to be more fancy

--- tools/controller/RequestAnalyzerTools.py
+++ tools/controller/RequestAnalyzerTools.py
@@ -1,40 +1,57 @@
 import user_agents
+from django.http import HttpRequest
 
+def get_ip(request:HttpRequest) -> str:
+  key_names = [
+    ('HTTP_X_FORWARDED_FOR', lambda ip: ip.split(',')[0]), 
+    ('REMOTE_ADDR', lambda ip: ip), 
+    ('HTTP_X_REAL_IP', lambda ip: ip)
+  ]
 
-def get_ip(request) -> str:
-  x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-  if x_forwarded_for:
-    ip = x_forwarded_for.split(',')[0]
-  else:
-    ip = request.META.get('REMOTE_ADDR')
+  ip = None
+  for key, cleaner in key_names:
+    ip = cleaner(request.META.get(key, ''))
+    if ip: break
+
   return ip
 
-def get_proxy_anonymity(request) -> str:
-  rename_header = lambda h: f'http-{h}'.upper().replace('-', '_')
-  proxy_headers = map(rename_header, [
+def get_proxy_anonymity(request:HttpRequest) -> str:
+  ELITE, ANONYMOUS, TRANSPARENT, NO_PROXY = 'elite', 'anonymous', 'transparent', 'none'
+
+  proxy_msgs = {
+    ELITE: 'Its {proxy_type} proxy (our server doesn\'t see any proxy, only see this ip -> {ip})',
+    ANONYMOUS: 'Its {proxy_type} proxy (telling its a proxy but not telling about your real ip -> {ip})',
+    TRANSPARENT: 'Its {proxy_type} proxy (telling its a proxy at -> {proxy_ip} , and telling about your real ip -> {ip})',
+    NO_PROXY: 'Didn\'t identify any proxy'
+  }
+  get_proxy_msg = lambda proxy_type, ip, proxy_ip: proxy_msgs[proxy_type].format(proxy_type=proxy_type, ip=ip, proxy_ip=proxy_ip)
+
+  proxy_headers = [
     # elite
     # Anonymous
-    'Authorization', 
-    'From', 
-    'Proxy-Authorization',
-    'Proxy-Connection',
-    'Via',
+    'HTTP_AUTHORIZATION', 'HTTP_FROM', 'HTTP_PROXY_AUTHORIZATION', 'HTTP_PROXY_CONNECTION', 'HTTP_VIA',
     # transparent
-    'X-Forwarded-For'
-  ])
+    'HTTP_X_FORWARDED_FOR'
+  ]
 
-  headers = request.META
-  found_headers = [h for h in proxy_headers if headers.get(h)]
+  proxy_ip = request.META.get('HTTP_X_FORWARDED_FOR', '').split(',')[0]
+  real_ip = request.META.get('REMOTE_ADDR') or request.META.get('HTTP_X_REAL_IP')
 
-  anonymity = 'anonymous'
-  if len(found_headers) == 0:
-    anonymity = 'elite'
-  elif rename_header('X-Forwarded-For') in found_headers:
-    anonymity = 'transparent'
+  proxy_type = ELITE
+  if proxy_ip:
+    """ Then its Transparent or Anonymous or None """
+    if proxy_ip != real_ip and real_ip: 
+      proxy_type = TRANSPARENT
+    elif len([h for h in proxy_headers if request.META.get(h)]):
+      proxy_type = ANONYMOUS
+    elif proxy_ip == real_ip:
+      proxy_type = NO_PROXY
+    else:
+      proxy_type = NO_PROXY
 
-  return anonymity
+  return get_proxy_msg(proxy_type, real_ip, proxy_ip)
 
-def get_request_headers(request) -> dict:
+def get_request_headers(request:HttpRequest) -> dict:
   headers = {}
 
   for key, value in request.META.items():
@@ -44,7 +61,7 @@ def get_request_headers(request) -> dict:
 
   return headers
 
-def get_user_agent_details(ua) -> dict:
+def get_user_agent_details(ua:str) -> dict:
   user_agent = user_agents.parse(ua)
   return {
     'ua': str(user_agent),
