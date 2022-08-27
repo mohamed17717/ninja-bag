42800ef53428c2f9c89d90688fbe664e8e35a7c8
d3v.mhmd <mohamad.mahmoud@florinz.com>
2022-08-27 03:06:43 +0200

    allow all subdomains

--- dj/settings.py
+++ dj/settings.py
@@ -8,10 +8,9 @@ SECRET_KEY = '$e5rh03te^#or+2dvix(9zgnp0*xjya92e3guwni)r30p73x4-'
 DEBUG = True
 
 ALLOWED_HOSTS = [
-  'ninja-bag.site',
   '139.162.134.211',
   'devmhmd.com',
-  'www.devmhmd.com',
+  '*.devmhmd.com',
 ]
 
 INSTALLED_APPS = [
